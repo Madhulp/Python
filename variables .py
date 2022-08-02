6a57198f75d070fcbbc4e12a2862712025fcaf65
madhul patil <madhulpatil7@gmail.com>
2022-08-03 01:30:09 +0530

    python

---
+++ variables .py
@@ -0,0 +1,33 @@
+a=2
+b=5
+print(a+b)
+
+
+
+a=1900
+b=17
+print(a%b)
+
+
+a="strow"
+print(type(a))
+
+
+a=34
+b=80
+print(a>b)
+
+
+a=input("a ")
+b=input("b ")
+a=int(a)
+b=int(b)
+avg=(a+b)/2
+print(avg)
+
+
+
+a=input("square of a number:")
+a=a*a
+a=int(a)
+print(a)
