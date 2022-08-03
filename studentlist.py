e91f5ba6451e229b7c8402a9e0c0a01f68af9296
madhul patil <madhulpatil7@gmail.com>
2022-08-03 23:19:37 +0530

    python

---
+++ studentlist.py
@@ -0,0 +1,7 @@
+stu1=int(input("marks of student 1 "))
+stu2=int(input("marks of student 2 "))
+stu3=int(input("marks of student 3 "))
+stu4=int(input("marks of student 4 "))
+marks=[stu1,stu2,stu3,stu4]
+marks.sort()
+print(marks)
