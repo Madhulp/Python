697773250f38f9ceb8b1b359536bd79b5934394e
madhul patil <madhulpatil7@gmail.com>
2022-08-07 11:42:45 +0530

    python

--- Loops/From A to B.py
+++ Loops/From A to B.py
@@ -1,6 +1,6 @@
-max,min=map(int,input().split())
-max1=max
-min1=min
-for x in range(min1,max1,1):
-    i=x
+max, min = map(int, input().split())
+max1 = max
+min1 = min
+for x in range(min1, max1, 1):
+    i = x
     print(i)
--- Project/Week Days.py
+++ Project/Week Days.py
@@ -1,11 +1,14 @@
-def dayofweek(y,m,d):
-    t=[0,3,2,5,0,3,5,1,4,6,2,4]
-    if (m<3):
-        y-=1
-    return (y+y//4-y//100+y//400+t[m-1]+d)%7;
-n=int(input())
+def dayofweek(y, m, d):
+    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+    if (m < 3):
+        y -= 1
+    return (y+y//4-y//100+y//400+t[m-1]+d) % 7
+
+
+n = int(input())
 for i in range(n):
-    d,m,y=map(int,input().split("/"))
-    a=dayofweek(y,m,d)
-    arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    d, m, y = map(int, input().split("/"))
+    a = dayofweek(y, m, d)
+    arr = ["Sunday", "Monday", "Tuesday",
+           "Wednesday", "Thursday", "Friday", "Saturday"]
     print(arr[a])
