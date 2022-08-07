5c7fef4aed57649b2b0eeedffdfd9e2511b9ad61
madhul patil <madhulpatil7@gmail.com>
2022-08-07 09:23:15 +0530

    python

--- greatest.py
+++ greatest.py
@@ -1,6 +1,11 @@
-list=["manu","nik","opppo"]
-lists=input("Enter names: ")
-if(lists in list):
-    print("Yes")
-else:
-    print("no")        
+# list=["manu","nik","opppo"]
+# lists=input("Enter names: ")
+# if(lists in list):
+#     print("Yes")
+# else:
+#     print("no")  
+
+
+
+for i in range(0,10,2):
+    print(i)      
