aad4de274f7ac9bae36a1b6b5d3fdae68519c0ef
madhul patil <madhulpatil7@gmail.com>
2022-08-20 19:13:20 +0530

    python

--- Area and Perimeter.py
+++ Area and Perimeter.py
@@ -1,13 +1,13 @@
-l1,b1,l2,b2=map(int,input().split())
-area1=l1*b1
-area2=l2*b2
-perimeter1=2*(l1+b1)
-perimeter2=2*(l2+b2)
-if area1>area2:
+l1, b1, l2, b2 = map(int, input().split())
+area1 = l1*b1
+area2 = l2*b2
+perimeter1 = 2*(l1+b1)
+perimeter2 = 2*(l2+b2)
+if area1 > area2:
     print("true")
 else:
     print("false")
-if perimeter1>perimeter2:
+if perimeter1 > perimeter2:
     print("true")
 else:
     print("false")
