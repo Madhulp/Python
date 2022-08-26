c4240ebb35d0196fa6b1b05a67c2b3c47478bb01
madhul patil <madhulpatil7@gmail.com>
2022-08-26 14:37:31 +0530

    Python

--- Lists/Noddy's Examination.py
+++ Lists/Noddy's Examination.py
@@ -1,12 +1,12 @@
-N,M=map(int,input().split(' '))
-diff_level=list(map(int,input().split()))[:N]
-marks=0
-skip=0
+N, M = map(int, input().split(' '))
+diff_level = list(map(int, input().split()))[:N]
+marks = 0
+skip = 0
 for i in range(N):
-    if diff_level[i]>M:
-        skip+=1
-        if skip>1:
+    if diff_level[i] > M:
+        skip += 1
+        if skip > 1:
             break
     else:
-        marks+=1
-print(marks)        
+        marks += 1
+print(marks)
